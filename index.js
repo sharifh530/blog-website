f0cb5b62b0a286271571765b5fa0ddb54322fe3c
SHARIF <sharifh530@gmail.com>
2020-07-03 03:27:54 +0600

    port updated

--- index.js
+++ index.js
@@ -86,7 +86,11 @@ Post.findOne({_id: requestedPostId}, function(err, post){
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server has started on sever 3000");
 });
